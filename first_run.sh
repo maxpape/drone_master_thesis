6ed0bc0f785baadf065daa698ad13fd4aafed363
Max Pape <maxpape@mail.uni-paderborn.de>
2024-02-16 10:34:27 +0000

    update initial setup process

--- first_run.sh
+++ first_run.sh
@@ -2,22 +2,19 @@
 
 
 git submodule update --init --recursive
-source /opt/ros/noetic/setup.bash
+#source /opt/ros/noetic/setup.bash
 bash ./PX4-Autopilot/Tools/setup/ubuntu.sh --no-sim-tools --no-nuttx
 
-source ./PX4-Autopilot/Tools/simulation/gazebo-classic/setup_gazebo.bash $(pwd)/PX4-Autopilot $(pwd)/PX4-Autopilot/build/px4_sitl_default
-export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/PX4-Autopilot
-export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/PX4-Autopilot/Tools/simulation/gazebo-classic/sitl_gazebo-classic
+#source ./PX4-Autopilot/Tools/simulation/gazebo-classic/setup_gazebo.bash $(pwd)/PX4-Autopilot $(pwd)/PX4-Autopilot/build/px4_sitl_default
+#export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/PX4-Autopilot
+#export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/PX4-Autopilot/Tools/simulation/gazebo-classic/sitl_gazebo-classic
 
 cd PX4-Autopilot
 DONT_RUN=1 make px4_sitl gazebo-classic
 
 cd ./..
 
-pip install virtualenv
 
-virtualenv acados_env
-source acados_env/bin/activate
 
 pip install -e ./acados/interfaces/acados_template
 pip install PyYAML
@@ -30,5 +27,5 @@ cmake -DACADOS_WITH_QPOASES=ON ..
 # add more optional arguments e.g. -DACADOS_WITH_OSQP=OFF/ON -DACADOS_INSTALL_DIR=<path_to_acados_installation_folder> above
 make install -j4
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"$(pwd)/acados/lib"
-export ACADOS_SOURCE_DIR="$(pwd)/acados"
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"$(pwd)/acados/lib"
+#export ACADOS_SOURCE_DIR="$(pwd)/acados"

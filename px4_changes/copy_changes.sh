74547f4c3f12743f9b3ad1146a1dbed34ffa9af0
Max Pape <maxpape@mail.uni-paderborn.de>
2024-03-04 12:41:49 +0000

    added avoidance module

--- px4_changes/copy_changes.sh
+++ px4_changes/copy_changes.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cp -r /drone_master_thesis/px4_changes/* /drone_master_thesis/PX4-Autopilot
+rsync -a --progress -v /drone_master_thesis/px4_changes/ /drone_master_thesis/PX4-Autopilot/ --exclude=copy_changes.sh
